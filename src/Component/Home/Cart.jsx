806a3ea539698aa373e9b20daeb68f91d21ae195
Tamal1 <tamalkrishna2026@gmail.com>
2023-09-15 00:12:26 +0600

    Handle Function Added to all cards

--- src/Component/Home/Cart.jsx
+++ src/Component/Home/Cart.jsx
@@ -1,9 +1,22 @@
 
 
-const Cart = () => {
+
+const Cart = ({selectedCourse}) => {
+
     return (
         <div>
-            
+          {
+            selectedCourse.map((course) =>{
+                <li className="text-black">{course.courseName}</li>
+            })
+          }
+              <h1>Credit Hour Remaining:</h1>
+                    <hr />
+                    <h1 className="font-bold text-xl pb-4">Course Name:{}</h1>
+                    <hr />
+                    <h1>Total Credit Hour:</h1>
+                    <hr />
+                    <h1>Total Price:</h1>
         </div>
     );
 };
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,52 +1,43 @@
-/* eslint-disable react/jsx-key */
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import Cart from "./Cart";
 
 const Home = () => {
     const [allCourse, setallCourse] = useState([]);
-    useEffect(()=>{
+    const [selectedCourse, setselectedCourse] = useState([]);
+    useEffect(() => {
         fetch("./info.json")
         .then(res => res.json())
         .then((data) => setallCourse(data))
-    } ,[]);
-    console.log(allCourse);
+    }, []);
+const handleCourse=(course)=>{
+setselectedCourse([...selectedCourse,course])
+};
+
     return (
-       <>
-        <div className="container ml-6 flex justify-between">
-            
-       {
-        allCourse.map(course =>( <div className="card-container ">
-        <div className="w-[350px] h-[380x] bg-white shadow-2xl rounded-xl">
-            <img className="p-6" src={course.img} alt="" />
-            <h1 className="font-bold pl-5">{course.courseName}</h1>
-
-            <p className="font-extralight text-gray-400 pl-3">{course.courseDetails}</p>
-            <div className="flex justify-around">
-            <h1>Price:{course.price}</h1>
-            <h1>Credit:{course.credit}Hr</h1>
+        <>
+            <div className="container flex ml-6">
+                <div className="flex flex-wrap mx-4">
+                    {allCourse.map(course => (
+                        <div key={course.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 px-4 mb-6">
+                            <div className="w-[350px] h-[400px] bg-white shadow-2xl rounded-xl">
+                                <img className="p-6" src={course.img} alt="" style={{ width: '400px', height: '250px' }} />
+                                <h1 className="font-bold pl-5">{course.courseName}</h1>
+                                <p className="font-extralight text-gray-400 pl-3">{course.courseDetails}</p>
+                                <div className="flex justify-around">
+                                    <h1>Price: {course.price}</h1>
+                                    <h1>Credit: {course.credit} Hr</h1>
+                                </div>
+                                <button className="bg-blue-500 text-white font-semibold w-10/12 text-center rounded-lg ml-7 my-3 py-1" onClick={()=>handleCourse(course)}>Select</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="cart-container w-[950px] h-[380px] px-4 bg-white shadow-2xl rounded-xl">
+                    <Cart selectedCourse ={selectedCourse}></Cart>
+                  
+                </div>
             </div>
-            <button className="bg-blue-500 text-white font-semibold w-10/12 text-center rounded-lg ml-7 my-3 py-1">Select</button>
-        </div>
-       
-
-        </div>))
-       }
-        <div className="cart-container w-[250px] h-[380px] px-4 bg-white shadow-2xl rounded-xl">
-
-        <h1>Credit Hour Remaining:</h1>
-        <hr />
-        <h1 className="font-bold text-xl pb-4"> Course Name:</h1>
-        <hr />
-        <h1>Total Credit Hour:</h1>
-        <hr />
-        <h1>Total Price:</h1>
-
-        </div>
-            </div>
-       
-       
-       
-       </>
+        </>
     );
 };
 
