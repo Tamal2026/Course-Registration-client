2e4d09ce83fa7c107510696fe6910ba46d58c3a1
Tamal1 <tamalkrishna2026@gmail.com>
2023-09-16 03:28:42 +0600

    Complete Assainment

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import Cart from "./Cart";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
+import { FiDollarSign} from 'react-icons/fi';
+import { BsBook} from 'react-icons/bs';
 
 
 const Home = () => {
@@ -51,7 +51,7 @@ const maxCredit = () => toast.warn("You reached maximum credit of courses");
 
     return (
         <>
-            <div className="container flex ml-6">
+            <div className="container flex ml-6 mt-8">
                 <div className="flex flex-wrap mx-4">
                     {allCourse.map(course => (
                         <div key={course.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 px-4 mb-6">
@@ -60,8 +60,8 @@ const maxCredit = () => toast.warn("You reached maximum credit of courses");
                                 <h1 className="font-bold pl-5">{course.courseName}</h1>
                                 <p className="font-extralight text-gray-400 pl-3">{course.courseDetails}</p>
                                 <div className="flex justify-around">
-                                    <h1>Price: {course.price}$</h1>
-                                    <h1>Credit: {course.credit} Hr</h1>
+                                    <h1 className="flex items-center gap-2"><FiDollarSign></FiDollarSign>Price: {course.price}</h1>
+                                    <h1 className="flex items-center gap-2"><BsBook></BsBook>Credit: {course.credit} Hr</h1>
                                 </div>
                                 <button className="bg-blue-500 text-white font-semibold w-10/12 text-center rounded-lg ml-7 my-3 py-1" onClick={()=>handleCourse(course)}>Select</button>
                             </div>
